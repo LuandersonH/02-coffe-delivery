cc830dd8a030f48fe1a9669d95235d68768646ac
LuandersonH <work.luanderson@gmail.com>
2024-12-15 22:43:37 -0300

    CoffeList inicio, html/css de seu header

---
+++ src/pages/Home/components/CoffeList/CoffeList.styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const CoffeListContainer = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 5.75rem 10rem;
+
+  .coffeListHeader {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > p {
+      display: inline-flex;
+
+      font-family: "Baloo 2", serif;
+      font-size: 2rem;
+      font-weight: bolder;
+
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+
+      > button {
+        background: transparent;
+        padding: 0.375rem 0.75rem;
+
+        border: 1px solid ${(props) => props.theme.yellow};
+        border-radius: 100px;
+
+        font-size: 10px;
+        font-weight: bold;
+        color: ${(props) => props.theme["yellow-dark"]};
+        
+        &:focus {
+            outline: none;
+            box-shadow: 0 0 0 0.5px ${(props) => props.theme['yellow-dark']};
+        }
+
+        &:active {
+            background: ${(props) => props.theme["yellow-light"]};
+        }
+
+        &:hover {
+            background: ${(props) => props.theme["yellow-light"]};
+            cursor: pointer;
+        }
+      }
+    }
+  }
+`;
--- src/pages/Home/components/CoffeList/CoffeList.tsx
+++ src/pages/Home/components/CoffeList/CoffeList.tsx
@@ -1,3 +1,63 @@
+import { CoffeListContainer } from "./CoffeList.styles";
+
 export function CoffeList() {
-  return <div>aqui vai ficar a lista de cafés</div>;
+  function TradicionalButton() {
+    // if (value === tradicional) = mostre os cards que tem o coffeType tradicional e oculte os outros
+  }
+
+  function EspecialButton() {}
+  function ComLeiteButton() {}
+  function AlcoólicoButton() {}
+  function GeladoButton() {}
+
+  return (
+    <CoffeListContainer>
+      <div className="coffeListHeader">
+        <p>Nossos cafés</p>
+        <div>
+          <button
+            onClick={() => {
+              TradicionalButton();
+            }}
+            value="TRADICIONAL"
+          >
+            TRADICIONAL
+          </button>
+          <button
+            onClick={() => {
+              EspecialButton();
+            }}
+            value="TRADICIONAL"
+          >
+            ESPECIAL
+          </button>
+          <button
+            onClick={() => {
+              ComLeiteButton();
+            }}
+            value="TRADICIONAL"
+          >
+            COM LEITE
+          </button>
+          <button
+            onClick={() => {
+              AlcoólicoButton();
+            }}
+            value="TRADICIONAL"
+          >
+            ALCOÓLICO
+          </button>
+          <button
+            onClick={() => {
+              GeladoButton();
+            }}
+            value="TRADICIONAL"
+          >
+            GELADO
+          </button>
+        </div>
+      </div>
+      <div>conteúdo da coffelist</div>
+    </CoffeListContainer>
+  );
 }
